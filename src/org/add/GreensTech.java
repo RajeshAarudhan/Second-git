656c7a730ea67446be041b77c3507347ce3577d9
RajeshAarudhan <mail2rajaesh@gmail.com>
2021-09-03 16:30:55 +0530

    second save

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+
+public class GreensTech {
+	
+	public void greensOmr() {
+		System.out.println("BRANCH 1:OMR");	}
+	
+	public void greensAdayar() {
+		System.out.println("BRANCH 2:ADAYAR");
+	}
+	public void greensTambaram() {
+		System.out.println("BRANCH 3:TAMBARAM");
+
+	}
+	public void greensVelacherry() {
+		System.out.println("BRANCH 4:VELACHERRY");
+
+	}
+	public void greensAnnaNagar() {
+		System.out.println("BRANCH 5:ANNA NAGAR");
+
+	}
+	public static void main(String[] args) {
+		GreensTech adrs = new GreensTech();
+		adrs.greensOmr();
+		adrs.greensAdayar();
+		adrs.greensTambaram();
+		adrs.greensVelacherry();
+		adrs.greensAnnaNagar();
+	}
+
+}
